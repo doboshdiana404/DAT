ef98338579ef662824fb8ee2298512d15d10e5e5
diana_dobosh <diana.lakatosh.girl@gmail.com>
2024-09-11 23:03:03 +0300

    fix category

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function handleCategoryChange() {
+    var selectedCategory = document.getElementById('categorySelect').value;
+    console.log('Вибрана категорія:', selectedCategory);
+    // Логіка для обробки вибору категорії
+  }
+  
